a6822623b70d22b443c6521b44766a3fb3d14a1c
EbenGitHub <abenezergoo@gmail.com>
2023-01-23 11:09:00 +0300

    pass fun w/out callin

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 const _ = require('lodash');
 
 $('body').append("<p>Holberton Dashboard</p>")
-        .append("<p>ashboard data for the students</p>")
+        .append("<p>Dashboard data for the students</p>")
         .append("<button>Click here to get started</button>")
         .append("<p id='count'></p>")
         .append("<p>Copyright - Holberton School</p>");
@@ -14,6 +14,6 @@ function updateCounter() {
     $("#count").text(`${count} clicks on the button`);
 }
 
-$("button").on( "click", updateCounter());
+$("button").on( "click", updateCounter);
 
 $("button").on("click", _.debounce(updateCounter, 500));
