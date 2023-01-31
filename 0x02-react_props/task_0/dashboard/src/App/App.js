c8b4e00b161b80a8fd919cb04463b5d02cac564f
EbenGitHub <abenezergoo@gmail.com>
2023-01-31 08:54:55 +0300

    setng up for proj

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,31 @@
+//import logo from './favicon.ico';
+import './App.css';
+import img from './holberton-logo.jpg'
+import { getFullYear, getFooterCopy } from './utils.js'
+
+let fullYear = getFullYear().toString();
+let footerCopy = getFooterCopy(true)
+
+function App() {
+  return (
+    <div>
+      <div className="App-header">
+        <img src={img} />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label for="email">Email</label>
+        <input type="email" id="email" />
+        <label for="password">Password</label>
+        <input type="password" id="password" />
+        <button value="OK">OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright { fullYear } - { footerCopy }</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
